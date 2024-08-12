d7c65f26ae87c953b426d1a51d76d83331907fc4
Vinícius Alves Borba <viniboal28@gmail.com>
2024-08-12 20:21:08 -0300

    feat: add update method to alternatives repository

--- src/repositories/alternatives-repository.ts
+++ src/repositories/alternatives-repository.ts
@@ -1,6 +1,10 @@
-import { Alternative } from '@prisma/client'
+import { Alternative, Prisma } from '@prisma/client'
 
 export interface AlternativesRepository {
   findById(id: string): Promise<Alternative | null>
   delete(id: string): Promise<void>
+  update(
+    id: string,
+    data: Prisma.AlternativeUncheckedUpdateInput,
+  ): Promise<void>
 }
